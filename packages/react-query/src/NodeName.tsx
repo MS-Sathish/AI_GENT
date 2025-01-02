d6a7bbb44121eb9380de8d439f66e5e982305ac8
SharmilaBlessyS <sharmila@kalakendradao.org>
2025-01-02 22:28:30 +0530

    new changes added

--- packages/react-query/src/NodeName.tsx
+++ packages/react-query/src/NodeName.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-import { useApi } from '@polkadot/react-hooks';
+// import { useApi } from '@polkadot/react-hooks';
 
 interface Props {
   children?: React.ReactNode;
